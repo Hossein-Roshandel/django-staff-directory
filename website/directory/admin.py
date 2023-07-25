583cad9afa12ace821abaa35ce024fd4d7034870
Hossein Roshandel <hossein.roshandel@gmail.com>
2023-07-26 07:01:16 +0800

    Add Staff migration

--- website/directory/admin.py
+++ website/directory/admin.py
@@ -2,4 +2,34 @@ from django.contrib import admin
 from .models import Staff
 # Register your models here.
 
-admin.site.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    
+
+    list_display = ('full_name', 'title', 'email', 'phone', 'office', 'is_active')
+    list_filter = ('title', 'is_active', "created_at", "updated_at")
+    search_fields = ('fname', 'lname', 'title', 'email', 'phone', 'office')
+    ordering = ('lname', 'fname', 'title')
+    readonly_fields = ('qrcode_image',"created_at", "updated_at", "created_by", "updated_by")
+    fieldsets = (
+        (None, {
+            'fields': ('fname', 'lname', 'title', 'email', 'phone', 'office', 'bio', 'image', 'qrcode_image')
+        }),
+        ('Record Status', {
+            'fields': ('is_active',),
+        }),
+        ('Record Tracking', {
+            'fields': ('created_at', 'updated_at', 'created_by', 'updated_by'),
+        }),
+    )
+
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        else:
+            obj.updated_by = request.user
+        obj.save()
+
+
+
+admin.site.register(Staff, StaffAdmin)
