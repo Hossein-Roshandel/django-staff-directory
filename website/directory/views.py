67d7f8b8213edd5182303d3f4b6b7adf650d5d9f
Hossein Roshandel <hossein.roshandel@gmail.com>
2023-07-28 11:55:20 +0800

    Add basic views for directory list

--- website/directory/views.py
+++ website/directory/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http.request import HttpRequest
+from .models import Staff
 # Create your views here.
 
-def index(request:HttpRequest):   
-    return HttpResponse("Hello, world. You're at the staff directory index.")
+def index(request:HttpRequest):
+
+    context = {'staffs': Staff.objects.all()}
+    return render(request, 'directory/index.html', context)
