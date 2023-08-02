884584bdcacd89f1cc6db924f162b4a10f052039
Hossein Roshandel <hossein.roshandel@gmail.com>
2023-08-03 02:48:22 +0800

    Add Navbar active link highlighting

--- website/website/context_processors.py
+++ website/website/context_processors.py
@@ -1,5 +1,15 @@
 from django.conf import settings
+from django.urls import reverse
 
 
 def extra_context(request):
-    return {"base_url": settings.DJANGO_BASE_URL}
+    return {
+        "base_url": settings.DJANGO_BASE_URL,
+        "home_url": reverse("index"),
+        "about_url": "#",
+        "contact_url": "#",
+        "login_url": reverse("admin:login"),
+        "logout_url": reverse("admin:logout"),
+        "dashboard_url": reverse("admin:index"),
+        "register_url": reverse("register"),
+    }
