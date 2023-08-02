b9fdaacecab2ee73395275b7ce7758b1910b9ace
Hossein Roshandel <hossein.roshandel@gmail.com>
2023-08-03 01:13:45 +0800

    Remove unused internal IP (maybe for K8s)

--- website/website/settings.py
+++ website/website/settings.py
@@ -44,7 +44,7 @@ if DEBUG:
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", ]
 
 
 # Application definition
