50257a830094a7d30081da356a6f7c8427710f58
alexw005 <fswoon2@gmail.com>
2024-11-21 22:16:45 +0800

    use products for post and get

--- website/products/urls.py
+++ website/products/urls.py
@@ -3,6 +3,6 @@ from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
 
 urlpatterns = [
     
-    path('create', ProductListCreateView.as_view(), name='product-list-create'),
+    path('', ProductListCreateView.as_view(), name='product-list-create'),
     path('<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
 ]
