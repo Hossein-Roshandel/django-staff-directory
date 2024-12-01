d3667a9c1286026859a90f6ce4c70628c149ba1e
alexw005 <fswoon2@gmail.com>
2024-12-01 11:29:33 +0800

    add category filter

--- website/products/views.py
+++ website/products/views.py
@@ -8,10 +8,22 @@ INDEX_PAGE_TEMPLATE = "products/index.html"
 
 # List all products or create a new one
 class GetProductListView(generics.ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given product,
+        by filtering against a `category` query parameter in the URL.
+        """
+        queryset = Product.objects.all()
+        category = self.request.query_params.get('category')
+        print("HELLO", category)
+        if category is not None:
+            queryset = queryset.filter(category=category)
+        return queryset
 
 # Retrieve, update, or delete a product
 class GetSpecificProductView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
