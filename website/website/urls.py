b86920e7175fc83dc6c0964757107046dc2b302f
alexw005 <fswoon2@gmail.com>
2025-01-11 17:59:55 +0800

    fix issue

--- website/website/urls.py
+++ website/website/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("prometheus/", include("prometheus.urls")),
     path("register/", include("register.urls")),
     path("products/", include("products.urls")),
-    path("banners/", include('banners.url')),
+    path("banner/", include('banner.urls')),
     # path('polls/', include('polls.urls')),
     path("", include("directory.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
